367e67de79be2b0439d5b3c76df1a275e5303d1e
FahadIqbal-01 <fahad.iqbal.im.47@gmail.com>
2024-01-22 16:12:59 +0600

    ntg

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fa-solid fa-angle-right right_arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-left left_arrow"></i>',
+});
+
+
+$('.slider_main').slick({
+    
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow:'<i class="fa-solid fa-angle-right right-arrow"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-left left-arrow"></i>',
+  });
